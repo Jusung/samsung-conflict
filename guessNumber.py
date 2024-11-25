5f05354d68dbfe7796813af2df810d89e20cb781
jusung <kyejusung@gmail.com>
2024-11-25 18:54:13 +0900

    implemented guess number

--- guessNumber.py
+++ guessNumber.py
@@ -1,5 +1,19 @@
 import random
 
 def guess_number():
-    
+    number = random.randint(1, 100)
+    attempts = 0
+    print("Guess the number (1-100):")
+
+    while True:
+        guess = int(input("Your guess: "))
+        attempts += 1
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+        else:
+            print(f"Correct! It took you {attempts} attempts.")
+            break
+
 guess_number()
