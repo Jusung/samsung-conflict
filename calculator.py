a0862a6ee891200108e6632b0c742393be6ed1e7
jusung <kyejusung@gmail.com>
2024-11-25 19:02:50 +0900

    implemented calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,21 @@
 def calculator():
-    
+    print("Simple Calculator")
+    num1 = float(input("Enter first number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/':
+        result = num1 / num2
+    else:
+        print("Invalid operation!")
+        return
+
+    print(f"The result is: {result}")
+
 calculator()
