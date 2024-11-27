881515e226f26f87cc49f25841eff82460a0c5ec
jusung <kyejusung@gmail.com>
2024-11-27 10:46:41 +0900

    ready to test

--- todolist.py
+++ todolist.py
@@ -1,21 +1,5 @@
 todo_list = []
 
-def add_task(task):
-    todo_list.append(task)
-    print(f"'{task}' added to the list!")
-
-def show_tasks():
-    print("\nTodo List:")
-    for idx, task in enumerate(todo_list, start=1):
-        print(f"{idx}. {task}")
-
-def remove_task(index):
-    if 0 <= index < len(todo_list):
-        removed = todo_list.pop(index)
-        print(f"'{removed}' removed from the list.")
-    else:
-        print("Invalid index!")
-
 # 실행 예제
 add_task("Learn Python")
 add_task("Do Git practice")
